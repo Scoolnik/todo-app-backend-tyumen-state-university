3874619dfe30bb9edcdc6c425d0acd9e3e493eeb
Whisper <mrromansimonov@gmail.com>
2023-05-09 19:28:24 +0500

    фикс названия поля

--- TODOAppBackend/Repository/ApplicationContext.cs
+++ TODOAppBackend/Repository/ApplicationContext.cs
@@ -5,8 +5,8 @@ namespace TODOAppBackend.Repository
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<TM_Task> Tasks { get; set; }
-        public DbSet<TM_User> Users { get; set; }
+        public DbSet<TM_Task> TM_Task { get; set; }
+        public DbSet<TM_User> TM_User { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
